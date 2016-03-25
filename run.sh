22b43c9c2d4d80ed7b76458df566e3f26c1d0a85
Jay R. Wren <jrwren@xmtp.net>
2016-03-25 08:43:43 -0400

    have uwsgi use python in virtualenv in .

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-uwsgi_python --socket 127.0.0.1:9001 -d uwsgi.log --module tco --callable app \
+uwsgi_python -H . --socket 127.0.0.1:9001 -d uwsgi.log --module tco --callable app \
 --stats 127.0.0.1:9002
